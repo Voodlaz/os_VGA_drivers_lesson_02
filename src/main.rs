7d14919049b67e718ab6e26f4c058ba23eb76f55
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-09-24 03:47:05 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
 
+mod vga_driver;
 extern crate rlibc;
 use core::panic::PanicInfo;
 
 #[no_mangle]
-fn _start() {}
+fn _start() {
+    vga_driver::writer("jml")
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
